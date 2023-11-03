2b2b99bcadd6dc61f4fe7deb79a5812b12fa1689
Tom Moran <62551154+Tomhausen@users.noreply.github.com>
2023-11-03 10:51:23 +0000

    Initial files for MakeCode project

---
+++ main.py
@@ -0,0 +1,110 @@
+@namespace
+class SpriteKind:
+    cannon = SpriteKind.create()
+
+# variables
+health = 0
+spawn_frequency = 7200
+info.set_score(500)
+
+# setup
+scene.set_tile_map_level(assets.tilemap("level"))
+scene.center_camera_at(96, 64)
+
+# sprites
+selector = sprites.create(assets.image("selector"), SpriteKind.player)
+selector.set_stay_in_screen(True)
+selector.z = 5
+grid.snap(selector)
+grid.move_with_buttons(selector)
+
+def make_level_text(level, cannon: Sprite):
+    level_text = textsprite.create(str(level), 15, 1)
+    level_text.set_position(cannon.x - 5, cannon.y + 4)
+    level_text.z = 4
+    sprites.set_data_sprite(cannon, "level_text", level_text)
+
+def upgrade_cannon(cannon: Sprite):
+    limit = sprites.read_data_number(cannon, "time_between_fire")
+    limit = Math.constrain(limit - 20, 50, 500)
+    sprites.set_data_number(cannon, "time_between_fire", limit)
+    sprites.change_data_number_by(cannon, "level", 1)
+    sprites.read_data_sprite(cannon, "level_text").destroy()
+    make_level_text(sprites.read_data_number(cannon, "level"), cannon)
+
+def buy_cannon(tile: tile.Location):
+    if tiles.tile_at_location_equals(tile, assets.tile("empty")):
+        cannon = sprites.create(assets.image("cannon"), SpriteKind.cannon)
+        tiles.place_on_tile(cannon, tile)
+        tiles.set_tile_at(tile, assets.tile("placed"))
+        cannon.z = 3
+        sprites.set_data_number(cannon, "frames_since_fired", 390)
+        sprites.set_data_number(cannon, "time_between_fire", 400)
+        sprites.set_data_number(cannon, "level", 1)
+        make_level_text(1, cannon)
+        info.change_score_by(-100)
+
+def interact():
+    if info.score() < 100:
+        return
+    tile = selector.tilemap_location()
+    cannons = spriteutils.get_sprites_within(SpriteKind.cannon, 1, selector)
+    if tiles.tile_at_location_equals(tile, assets.tile("placed")):
+        info.change_score_by(-100)
+        upgrade_cannon(cannons[0])
+    else:
+        buy_cannon(tile) 
+controller.A.on_event(ControllerButtonEvent.PRESSED, interact)
+
+def hit(cannon_ball, enemy):
+    sprites.change_data_number_by(enemy, "hp", -1)
+    if sprites.read_data_number(enemy, "hp") < 1:
+        enemy.destroy()
+        info.change_score_by(100)
+    cannon_ball.destroy()
+sprites.on_overlap(SpriteKind.projectile, SpriteKind.enemy, hit)
+
+def destroy_cannon(cannon, enemy):
+    tiles.set_tile_at(cannon.tilemap_location(), assets.tile("empty"))
+    sprites.read_data_sprite(cannon, "level_text").destroy()
+    cannon.destroy()
+    enemy.destroy()
+sprites.on_overlap(SpriteKind.cannon, SpriteKind.enemy, destroy_cannon)
+
+def game_over():
+    game.over(False)
+scene.on_overlap_tile(SpriteKind.enemy, assets.tile("game over"), game_over)
+
+def difficulty_increase():
+    global health, spawn_frequency
+    health = Math.constrain(health + 1, 0, 20)
+    spawn_frequency = Math.constrain(spawn_frequency - 100, -200, 10000)
+game.on_update_interval(20000, difficulty_increase)
+
+def spawn_enemy():
+    enemy = sprites.create(assets.image("ghost"), SpriteKind.enemy)
+    tiles.place_on_random_tile(enemy, assets.tile("spawn"))
+    enemy.vx = -7
+    sprites.set_data_number(enemy, "hp", health)
+    timer.after(spawn_frequency, spawn_enemy)
+spawn_enemy()
+
+def fire(cannon: Sprite):
+    ball = sprites.create(assets.image("cannon ball"), SpriteKind.projectile)
+    ball.set_position(cannon.x + 4, cannon.y)
+    ball.z = 0
+    ball.set_velocity(50, 0)
+    ball.set_flag(SpriteFlag.AUTO_DESTROY, True)
+    ball.set_flag(SpriteFlag.STAY_IN_SCREEN, False)
+    sprites.set_data_number(cannon, "frames_since_fired", 0)
+
+def fire_control():
+    for cannon in sprites.all_of_kind(SpriteKind.cannon):
+        sprites.change_data_number_by(cannon, "frames_since_fired", 1)
+        count = sprites.read_data_number(cannon, "frames_since_fired")
+        if count > sprites.read_data_number(cannon, "time_between_fire"):
+            fire(cannon)
+
+def tick():
+    fire_control()
+game.on_update(tick)
---
+++ main.ts
@@ -0,0 +1,121 @@
+namespace SpriteKind {
+    export const cannon = SpriteKind.create()
+}
+
+//  variables
+let health = 0
+let spawn_frequency = 7200
+info.setScore(500)
+//  setup
+scene.setTileMapLevel(assets.tilemap`level`)
+scene.centerCameraAt(96, 64)
+//  sprites
+let selector = sprites.create(assets.image`selector`, SpriteKind.Player)
+selector.setStayInScreen(true)
+selector.z = 5
+grid.snap(selector)
+grid.moveWithButtons(selector)
+function make_level_text(level: number, cannon: Sprite) {
+    let level_text = textsprite.create("" + level, 15, 1)
+    level_text.setPosition(cannon.x - 5, cannon.y + 4)
+    level_text.z = 4
+    sprites.setDataSprite(cannon, "level_text", level_text)
+}
+
+function upgrade_cannon(cannon: Sprite) {
+    let limit = sprites.readDataNumber(cannon, "time_between_fire")
+    limit = Math.constrain(limit - 20, 50, 500)
+    sprites.setDataNumber(cannon, "time_between_fire", limit)
+    sprites.changeDataNumberBy(cannon, "level", 1)
+    sprites.readDataSprite(cannon, "level_text").destroy()
+    make_level_text(sprites.readDataNumber(cannon, "level"), cannon)
+}
+
+function buy_cannon(tile: any) {
+    let cannon: Sprite;
+    if (tiles.tileAtLocationEquals(tile, assets.tile`empty`)) {
+        cannon = sprites.create(assets.image`cannon`, SpriteKind.cannon)
+        tiles.placeOnTile(cannon, tile)
+        tiles.setTileAt(tile, assets.tile`placed`)
+        cannon.z = 3
+        sprites.setDataNumber(cannon, "frames_since_fired", 390)
+        sprites.setDataNumber(cannon, "time_between_fire", 400)
+        sprites.setDataNumber(cannon, "level", 1)
+        make_level_text(1, cannon)
+        info.changeScoreBy(-100)
+    }
+    
+}
+
+controller.A.onEvent(ControllerButtonEvent.Pressed, function interact() {
+    if (info.score() < 100) {
+        return
+    }
+    
+    let tile = selector.tilemapLocation()
+    let cannons = spriteutils.getSpritesWithin(SpriteKind.cannon, 1, selector)
+    if (tiles.tileAtLocationEquals(tile, assets.tile`placed`)) {
+        info.changeScoreBy(-100)
+        upgrade_cannon(cannons[0])
+    } else {
+        buy_cannon(tile)
+    }
+    
+})
+sprites.onOverlap(SpriteKind.Projectile, SpriteKind.Enemy, function hit(cannon_ball: Sprite, enemy: Sprite) {
+    sprites.changeDataNumberBy(enemy, "hp", -1)
+    if (sprites.readDataNumber(enemy, "hp") < 1) {
+        enemy.destroy()
+        info.changeScoreBy(100)
+    }
+    
+    cannon_ball.destroy()
+})
+sprites.onOverlap(SpriteKind.cannon, SpriteKind.Enemy, function destroy_cannon(cannon: Sprite, enemy: Sprite) {
+    tiles.setTileAt(cannon.tilemapLocation(), assets.tile`empty`)
+    sprites.readDataSprite(cannon, "level_text").destroy()
+    cannon.destroy()
+    enemy.destroy()
+})
+scene.onOverlapTile(SpriteKind.Enemy, assets.tile`game over`, function game_over() {
+    game.over(false)
+})
+game.onUpdateInterval(20000, function difficulty_increase() {
+    
+    health = Math.constrain(health + 1, 0, 20)
+    spawn_frequency = Math.constrain(spawn_frequency - 100, -200, 10000)
+})
+function spawn_enemy() {
+    let enemy = sprites.create(assets.image`ghost`, SpriteKind.Enemy)
+    tiles.placeOnRandomTile(enemy, assets.tile`spawn`)
+    enemy.vx = -7
+    sprites.setDataNumber(enemy, "hp", health)
+    timer.after(spawn_frequency, spawn_enemy)
+}
+
+spawn_enemy()
+function fire(cannon: Sprite) {
+    let ball = sprites.create(assets.image`cannon ball`, SpriteKind.Projectile)
+    ball.setPosition(cannon.x + 4, cannon.y)
+    ball.z = 0
+    ball.setVelocity(50, 0)
+    ball.setFlag(SpriteFlag.AutoDestroy, true)
+    ball.setFlag(SpriteFlag.StayInScreen, false)
+    sprites.setDataNumber(cannon, "frames_since_fired", 0)
+}
+
+function fire_control() {
+    let count: number;
+    for (let cannon of sprites.allOfKind(SpriteKind.cannon)) {
+        sprites.changeDataNumberBy(cannon, "frames_since_fired", 1)
+        count = sprites.readDataNumber(cannon, "frames_since_fired")
+        if (count > sprites.readDataNumber(cannon, "time_between_fire")) {
+            fire(cannon)
+        }
+        
+    }
+}
+
+game.onUpdate(function tick() {
+    fire_control()
+})
